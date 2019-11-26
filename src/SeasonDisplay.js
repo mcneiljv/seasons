ee0c46465545f073303fefa84c996b71cf5f8823
Jamie <mcneiljv@sunypoly.edu>
2019-11-25 22:00:54 -0500

    Added additional consts to SeasonDisplay.js and SeasonDisplay.css

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,19 +1,35 @@
 import React from 'react';
 
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach!",
+        iconName: "sun"
+    },
+    winter: {
+        text: "Burr, it's cold!",
+        iconName: "snowflake"
+    }
+};
+
 const getSeason = (lat, month) => {
     if (month < 2 && month < 9) {
         return lat > 0 ? 'summer' : 'winter';
     } else {
         return lat > 0 ? 'winter' : 'summer';
     }
-}
+};
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
-    const text = season === 'winter' ? 'Burr, it is cold!' : 'Let us hit the beach!'
+    // Will return text and iconName
+    const { text, iconName } = seasonConfig[season];
 
     return (
-<h1>{text}</h1>
+    <div>
+        <i className={`icon-left massive ${iconName} icon`} />
+        <h1>{text}</h1>
+        <i className={`icon-right massive ${iconName} icon`} />
+    </div>
     );
 };
 
